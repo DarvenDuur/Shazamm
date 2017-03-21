0142dbc0e69d72a536b7ffb78e8518cd9e170417
Reiji7 <adrien_claudel@icloud.com>
2017-03-21 18:02:26 +0100

    Rezilliance.apply()

--- Shazamm/src/game/cards/Rezilliance.java
+++ Shazamm/src/game/cards/Rezilliance.java
@@ -5,7 +5,7 @@
  */
 package game.cards;
 
-import game.Bridge;
+import game.Round;
 
 /**
  *
@@ -13,10 +13,23 @@ import game.Bridge;
  */
 public class Rezilliance extends AbstractCard {
  
-    
-    
+    /**
+     * If the wall of fire were to advance towards the player, it does not move.
+     * @param round
+     * 
+     * @author Adrien
+     */
     @Override
-    public void apply(Bridge bridge){
-        
+    public void apply(Round round) {
+        //Application for player 1
+        if(isBelongPlayer1() && !round.getSecondLastTurn().getIsPlayer1Winner()){
+            round.getLastTurn().getBridge().getPlayerState1().setMana(
+                round.getLastTurn().getBridge().getPlayerState1().getBet());
+        }
+        //Application for player 2
+        else if(!isBelongPlayer1() && round.getSecondLastTurn().getIsPlayer1Winner()){
+            round.getLastTurn().getBridge().getPlayerState2().setMana(
+                round.getLastTurn().getBridge().getPlayerState2().getBet());
+        }
     }
 }
