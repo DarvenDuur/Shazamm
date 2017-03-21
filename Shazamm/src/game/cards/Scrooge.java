8e8e40ab9efb0b5bfcc45f1e162b7abea87309de
Reiji7 <adrien_claudel@icloud.com>
2017-03-21 16:43:33 +0100

    Scrooge.apply()

--- Shazamm/src/game/cards/Scrooge.java
+++ Shazamm/src/game/cards/Scrooge.java
@@ -5,18 +5,31 @@
  */
 package game.cards;
 
-import game.Bridge;
+import game.Round;
 
 /**
  *
  * @author darven
  */
 public class Scrooge extends AbstractCard {
- 
-    
-    
+
+    /**
+     * If this turn is lost, the stake is not removed from my Mana reserve.
+     * @param round
+     * 
+     * @author Adrien
+     */
     @Override
-    public void apply(Bridge bridge){
-        
+    public void apply(Round round) {
+        //Application for player 1
+        if(isBelongPlayer1()){
+            round.getLastTurn().getBridge().getPlayerState1().setMana(
+                round.getLastTurn().getBridge().getPlayerState1().getBet());
+        }
+        //Application for player 2
+        else{
+            round.getLastTurn().getBridge().getPlayerState2().setMana(
+                round.getLastTurn().getBridge().getPlayerState2().getBet());
+        }
     }
 }
