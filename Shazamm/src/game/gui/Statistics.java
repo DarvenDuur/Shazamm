08872d397d79bccb26f6841328d6940ff24ba0c7
Adrien <adrien_claudel@icloud.com>
2017-03-30 14:39:27 +0200

    Update Board and Statistics panel creation and resize new main Frame

---
+++ Shazamm/src/game/gui/Statistics.java
@@ -0,0 +1,24 @@
+package game.gui;
+
+
+/**
+ *
+ * @author Reiji
+ */
+public class Statistics extends javax.swing.JPanel {
+
+    /**
+     * Creates new form Statistics
+     */
+    public Statistics() {
+        initComponents();
+    }
+
+
+    private void initComponents() {
+
+        setBackground(new java.awt.Color(250, 0, 0));
+        setPreferredSize(new java.awt.Dimension(300, 400));
+    }
+
+}
---
+++ Shazamm/src/game/gui/test.java
@@ -0,0 +1,20 @@
+package game.gui;
+
+public class test extends javax.swing.JPanel {
+	
+    /**
+     * Creates new panel of test
+     */
+    public test() {
+        initComponents();
+    }
+
+
+    private void initComponents() {
+
+        setBackground(new java.awt.Color(189,90,10));
+        setPreferredSize(new java.awt.Dimension(500, 200));
+        
+        this.setBorder(javax.swing.BorderFactory.createLoweredBevelBorder());
+    }
+}
