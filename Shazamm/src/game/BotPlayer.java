a41970c0c1eae8bd3e23e21ba38219785b3826b5
mg <mg@MG-Linux>
2017-04-12 16:55:09 +0200

    add botPlayer class

---
+++ Shazamm/src/game/BotPlayer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+/**
+ *
+ * @author mg
+ */
+public class BotPlayer extends Player {
+    
+    public BotPlayer(boolean green) {
+        super(Config.AI_NAME, green);
+    }
+    
+    
+    
+}
