5e722e94d270eba213cd2cc4de94fc6c9d211581
DarvenDuur <e.marquer1@gmail.com>
2017-05-07 23:19:03 +0200

    AIAction

--- Shazamm/src/game/ai/AIAction.java
+++ Shazamm/src/game/ai/AIAction.java
@@ -1,7 +1,5 @@
-
 package game.ai;
 
-import game.Config;
 import game.Turn;
 import game.cards.AbstractCard;
 import game.cards.CardManager;
@@ -9,7 +7,7 @@ import game.cards.Clone;
 import java.util.HashSet;
 
 /**
- * Contains cards to play and bet to ... bet
+ * Contains cards to play and bet to bet
  */
 public class AIAction {
     //Cards to play (with clones already defined)
@@ -18,17 +16,6 @@ public class AIAction {
     //Bet to play
     private final int BET;
 
-    /**
-     * @param CARDS
-     *      Cards to play this turn
-     * @param BET 
-     *      Mana to bet this turn
-     */
-    public AIAction(HashSet<AbstractCard> CARDS, int BET) {
-        this.CARDS = CARDS;
-        this.BET = BET;
-    }
-    
     /**
      * @return 
      *      Mana to bet this turn
@@ -45,15 +32,31 @@ public class AIAction {
         return this.CARDS;
     }
 
-//***************************** asAIAction *************************************
+    /**
+     * Generate AIAction from fact base and turn
+     * @param factBase
+     *      fact base to analyse
+     * @param turn 
+     *      reference turn for cards
+     */
     public AIAction(FactBase factBase, Turn turn) {
+        //extract mana
         int availableMana = turn.getPlayerState(false).getMana();
         this.BET = factBase.extractBet(availableMana);
+        
+        //extract cards
         this.CARDS = new HashSet<>();
         this.extractCards(turn, factBase);
     }
 
-    private void extractClone(Turn turn, FactBase factBase, int clone) {
+    /**
+     * extract cloned card if any
+     * @param turn
+     *      turn from wich extract the cards
+     * @param clone 
+     *      cloned card id (if negative connsider double clone)
+     */
+    private void extractClone(Turn turn, int clone) {
         //if a card is cloned
         if (clone != 0) {
             CardManager botCardManager = turn.getPlayerState(false).getCardManager();
@@ -83,6 +86,15 @@ public class AIAction {
         }
     }
 
+    /**
+     * extract card from given set of card
+     * @param id
+     *      id of the card to extract
+     * @param availableCards
+     *      available cards
+     * @return 
+     *      extracted card
+     */
     private AbstractCard extractCard(int id, HashSet<AbstractCard> availableCards) {
         for (AbstractCard card : availableCards) {
             if (card.getId() == id) {
@@ -92,6 +104,13 @@ public class AIAction {
         return null;
     }
 
+    /**
+     * extract all cards from turn depending on fact base
+     * @param turn
+     *      turn from wich extract the cards
+     * @param factBase 
+     *      fact base to analyse
+     */
     private void extractCards(Turn turn, FactBase factBase) {
         int clone = factBase.getClone();
         int id;
@@ -99,20 +118,25 @@ public class AIAction {
         CardManager botCardManager = turn.getPlayerState(false).getCardManager();
         HashSet<AbstractCard> botCards = botCardManager.getHand();
         
+        // browse all fact for CardFacts
         for (Fact fact : factBase) {
             if (fact instanceof CardFact) {
                 CardFact cardFact = (CardFact) fact;
                 
+                // if fact describes the use of a card
                 if (cardFact.getType() == CardFact.USE) {
                     id = cardFact.getCardID();
                     
-                    //if card is not a cloned card
+                    /* if card is not a cloned card, it's added dirrectly to the 
+                    cards to play */
                     if (id != clone && id != clone) {
                         this.CARDS.add(this.extractCard(id, botCards));
                     }
                 }
             }
         }
-        this.extractClone(turn, factBase, clone);
+        
+        // extract cloned card if any
+        this.extractClone(turn, clone);
     }
 }
