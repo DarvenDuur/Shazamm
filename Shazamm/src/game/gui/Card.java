8dfac798f051af2bd468b13704ebcc74fceaf56e
DarvenDuur <e.marquer1@gmail.com>
2017-05-07 16:24:16 +0200

    repair previous comits

--- Shazamm/src/game/gui/Card.java
+++ Shazamm/src/game/gui/Card.java
@@ -36,8 +36,7 @@ public class Card extends javax.swing.JPanel {
     	main = new JPanel();
     	main.setBackground(new Color(0, 0, 0));
 
-        main.setMinimumSize(new Dimension(700, 350));
-        main.setMinimumSize(new Dimension(2700, 350));
+        main.setMaximumSize(new Dimension(2700, 350));
         
         this.setPreferredSize(new Dimension(700, 350));
         
@@ -58,14 +57,13 @@ public class Card extends javax.swing.JPanel {
         	j.setPreferredSize(new Dimension(220, 300));
         	j.setLayout(new FlowLayout());
         	
-        	Cards.add(new ImageIcon(GuiConfig.PATH_IMG + "cartes/0" + i + "v.jpg").getImage());
+        	Cards.add(new ImageIcon(GuiConfig.PATH_IMG + "/cartes/0" + i + "v.jpg").getImage());
         	chbx.add(new JCheckBox());
 
-                Image icon = new ImageIcon(GuiConfig.PATH_IMG + "cartes/0" + i + "v.jpg").getImage().getScaledInstance(200, 250, Image.SCALE_DEFAULT);
+        	Image icon = new ImageIcon(GuiConfig.PATH_IMG + "/cartes/0" + i + "v.jpg").getImage().getScaledInstance(200, 250, Image.SCALE_DEFAULT);
         	
         	JLabel lab = new JLabel();
         	lab.setIcon(new ImageIcon(icon));
-        	lab.setBorder(javax.swing.BorderFactory.createRaisedBevelBorder());
 
         	JCheckBox c = new JCheckBox();
         	c.setBackground(new Color(0, 0, 0));
